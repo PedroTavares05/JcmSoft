cebe527792f295fe2b0de97102f27b5823a6543e
Pedro Henrique Tavares Santos <phtsantos-5@gmail.com>
2026-08-14 21:14:19 -0300

    fix Departamento adicionando a DepartamentoId como cheve primaria

--- JcmSoft.Domain/Entities/Departamento.cs
+++ JcmSoft.Domain/Entities/Departamento.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace JcmSoft.Domain.Entities;
 
 public class Departamento
 {
+
+    [Key]
     public int DepartemntoId { get; set; }
     public string Nome { get; set; }
     public string Descricao { get; set; }
